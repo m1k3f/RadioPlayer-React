902bfeaefa82527231596f990de7f7309ae44f6c
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-01-01 23:37:53 -0600

    context updates

--- ClientApp/src/components/context/RadioContext.js
+++ ClientApp/src/components/context/RadioContext.js
@@ -5,17 +5,61 @@ const RadioContext = React.createContext();
 //export const RadioConsumer = RadioContext.Consumer;
 
 class RadioProvider extends Component {
+    constructor() {
+        super();
+        this.state = {
+            selectedStation: null,
+            radioPlaylist: this.getPlaylistStorage()
+        };
+    }
+
+    setStation = (station) => {
+        this.setState({
+            selectedStation: station
+        });
+    }
+
+    getPlaylistStorage = () => {
+        let radioPlaylist = null;
+        if (window.localStorage) {
+            radioPlaylist = localStorage.getItem('radioPlaylist');
+            if (radioPlaylist === undefined || radioPlaylist === null || radioPlaylist === '') {
+                radioPlaylist = {
+                    settings: {},
+                    playlist: []
+                };
+            }
+            else {
+                playlist = JSON.parse(playlist);
+            }
+        }
+
+        return playlist;
+    }
+
+    saveAndRefreshPlaylist = (playlist) => {
+        if (window.localStorage) {
+            localStorage.setItem("radioPlaylist", JSON.stringify(playlist));
+        }
+
+        this.setState({
+            radioPlaylist: this.getPlaylistStorage()
+        });
+    }
 
     render() {
         const { children } = this.props
-        const {  } = this.state
-        const {  } = this
+        const { selectedStation, radioPlaylist } = this.state
+        const { setStation, saveAndRefreshPlaylist } = this
 
         return(
             <RadioContext.Provider 
                 value={
                     {
-                        
+                        selectedStation,
+                        setStation,
+                        radioPlaylist,
+                        saveAndRefreshPlaylist
                     }
                 }
                 >
