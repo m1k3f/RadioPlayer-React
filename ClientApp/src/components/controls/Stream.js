de8417a2dce3236d9097b88f4eaa04000fbaa7bf
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-01-06 13:40:51 -0600

    stream style update

--- ClientApp/src/components/controls/Stream.js
+++ ClientApp/src/components/controls/Stream.js
@@ -3,15 +3,30 @@ import RadioContext from '../context/RadioContext';
 
 export default class Stream extends Component {
 
+    state = {
+        streamSrc: null
+    }
+
     static contextType = RadioContext;
 
     handlePlay = (e) => {
         // this.streamer.play();
     }
 
-    render() {
+    render() {        
+        let streamStyle = {
+            width: 0,
+            height: 0
+        };
+        if (this.state.streamSrc != null) {
+            streamStyle = {
+                width: '200px',
+                height: '200px'
+            };
+        }
+
         return(
-            <section className="stream">
+            <section className="stream" style={streamStyle}>
                 <video ref={el => this.streamer = el} onPlay={this.handlePlay} />
             </section>
         );
