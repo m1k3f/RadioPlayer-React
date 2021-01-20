788340a1e0697cebd2652c18132e229cb326f5b5
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-01-20 15:53:46 -0600

    station image bug fix

--- ClientApp/src/components/navbar/controls/StationImage.js
+++ ClientApp/src/components/navbar/controls/StationImage.js
@@ -5,6 +5,7 @@ import RadioContext from '../../context/RadioContext';
 export default class StationImage extends Component {
 
     state = {
+        faviconSrc: null,
         imageSrc: null
     }
 
@@ -12,18 +13,35 @@ export default class StationImage extends Component {
 
     async componentDidUpdate() {
         const { selectedStation } = this.context;
+        if (this.changeImage(selectedStation)) {
+            let imageObject = await this.getImage(selectedStation.station);
+            let imageSrc = null;
+            if (imageObject != null) {
+                imageSrc = `data:${imageObject.imageFileType};base64,${imageObject.imageBytes}`;
+            }
+
+            this.setState({
+                faviconSrc: selectedStation.station.favicon,
+                imageSrc: imageSrc
+            });
+        }
+    }
+
+    changeImage = (selectedStation) => {
+        let change = false;
         if (this.state.imageSrc === null && selectedStation.station !== null && 
             selectedStation.station.favicon.length > 0) {
-                let imageObject = await this.getImage(selectedStation.station);
-                let imageSrc = null;
-                if (imageObject != null) {
-                    imageSrc = `data:${imageObject.imageFileType};base64,${imageObject.imageBytes}`;
-                }
-
-                this.setState({
-                    imageSrc: imageSrc
-                });
+                //image set for the first time
+                change = true;
         }
+        else if (this.state.imageSrc !== null && selectedStation.station !== null && 
+                selectedStation.station.favicon.length > 0 && 
+                this.state.faviconSrc !== selectedStation.station.favicon) {
+                    //image set before but station was changed; change image to new station
+                    change = true;
+        }
+
+        return change;
     }
 
     getImage = async (station) => {
