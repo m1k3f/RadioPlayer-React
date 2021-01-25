298c7e3635383fe7c62a1d7d6d90944123db2c4f
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-01-24 19:17:15 -0600

    playlist import initial checkin

--- ClientApp/src/components/playlist/controls/PlaylistImportButton.js
+++ ClientApp/src/components/playlist/controls/PlaylistImportButton.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 
 export default class PlaylistImportButton extends Component {
 
-    handleButtonClick = (e) => {
+    handleImportButtonClick = (e) => {
+        this.fileImport.click();
+    }
 
+    handleFiles = (e) => {
+        let file = e.target.files[0];
+        if (file) {
+
+        }
     }
 
     render() {
         return(
-            <button className="iconButton" onClick={this.handleButtonClick} title="Import">
-                <i className="fas fa-upload"></i>
-            </button>
+            <React.Fragment>
+                <input type="file" id="playlistFile" style={{display:'none'}} accept=".pls"
+                        ref={el => this.fileImport = el} onChange={this.handleFiles} />
+                <button className="iconButton" onClick={this.handleImportButtonClick} title="Import">
+                    <i className="fas fa-upload"></i>
+                </button>                
+                {/* <label for="playlistFile" className="iconButton">
+                    <i className="fas fa-upload fa-sm"></i>
+                </label> */}
+            </React.Fragment>
         );
     }
 }
