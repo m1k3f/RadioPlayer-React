c70ccd3fe08eb4a566f90cd14b5bf6af8056044f
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-01-28 15:33:45 -0600

    logic updates for playlist type image

--- ClientApp/src/components/playlist/controls/StationTypeImage.js
+++ ClientApp/src/components/playlist/controls/StationTypeImage.js
@@ -4,17 +4,25 @@ export default class StationTypeImage extends Component {
 
     renderImage = () => {
         let content = null;
-        let codec = this.props.station.codec.toLowerCase();
-        let tags = this.props.station.tags.split(",");
-        if (codec === 'mp3' || codec === 'ogg' || codec === 'wav') {
-            content = <i className="fas fa-headphones"></i>
-        }
-        else if (tags.includes("video") || tags.includes("hls video")) {
+        let codec = (this.props.station.codec != undefined) ? this.props.station.codec.toLowerCase() : '';
+        let tags = (this.props.station.tags != undefined) ? this.props.station.tags.split(",") : '';
+
+        if (tags.includes("video") || tags.includes("hls video")) {
             content = <i className="fas fa-video"></i>
         }
         else {
-            content = <i className="fas fa-question-circle"></i>
+            content = <i className="fas fa-headphones"></i>
         }
+
+        // if (codec === 'mp3' || codec === 'ogg' || codec === 'wav') {
+        //     content = <i className="fas fa-headphones"></i>
+        // }
+        // else if (tags.includes("video") || tags.includes("hls video")) {
+        //     content = <i className="fas fa-video"></i>
+        // }
+        // else {
+        //     content = <i className="fas fa-question-circle"></i>
+        // }
 
         return (content);
     }
