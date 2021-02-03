67ed8d01bd82644bb05ac595f26adb676b261b9b
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-02-03 17:02:26 -0600

    warning icon for problem streams

--- ClientApp/src/components/search/SearchResultItem.js
+++ ClientApp/src/components/search/SearchResultItem.js
@@ -5,9 +5,34 @@ import TagList from './controls/TagList';
 
 export default class SearchResultItem extends Component {
 
-    render() {
-        let resultItem = this.props.resultItem;
+    renderCodecBitrate = (resultItem) => {
+        let content = null;
         let bitrate = (resultItem.bitrate > 0) ? `${resultItem.bitrate}kbps` : '';
+
+        if (resultItem.lastCheckOk) {
+            content = (
+                <p>{resultItem.codec} {bitrate}</p>
+            );
+        }
+        else {
+            let warningTitle = 'This stream has been experiencing problems.';
+            let warningStyle = {
+                color: 'red'
+            }
+            content = (
+                <p>
+                    <i className="fas fa-exclamation-triangle" style={warningStyle} title={warningTitle}></i>&nbsp;
+                    {resultItem.codec} 
+                    {bitrate}
+                </p>
+            );
+        }
+
+        return (content);
+    }
+
+    render() {
+        let resultItem = this.props.resultItem;        
         let state = (resultItem.state.length > 0) ? `${resultItem.state} - ` : '';
 
         let resultItemStyle = {};
@@ -26,7 +51,7 @@ export default class SearchResultItem extends Component {
                     </a>
                     <TagList tagList={resultItem.tags} />
                     <p>{state}{resultItem.country}</p>
-                    <p>{resultItem.codec} {bitrate}</p>                    
+                    {this.renderCodecBitrate(resultItem)}                   
                 </div>
             </div>
         );
