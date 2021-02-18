c8ccbb5263a8fc496ebf6a8b04762a19dc68ae6d
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-02-17 21:01:37 -0600

    Content update using css module

--- ClientApp/src/components/Content.js
+++ ClientApp/src/components/Content.js
@@ -1,13 +1,15 @@
 ﻿import React, { Component } from 'react';
-import Stream from './controls/Stream';
+
+import Stream from './stream/Stream';
 import Playlist from './playlist/Playlist';
 import StationSearch from './search/StationSearch'
+import styles from './Content.module.css';
 
 export default class Content extends Component {
 
     render() {
         return (
-            <main>
+            <main className={styles.main}>
                 <Stream />
                 <Playlist />
                 <StationSearch />
