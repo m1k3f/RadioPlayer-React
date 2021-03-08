ba9e0661ec241c91c4f50a46648afb242c1b7162
m1k3f <41029862+mikedr0p@users.noreply.github.com>
2021-03-08 15:11:24 -0600

    removed fontawesome css

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Radio from './components/Radio'
 
 import './css/animation.css'
-import './css/fontawesome/all.min.css';
 
 export default class App extends Component {
   static displayName = App.name;
